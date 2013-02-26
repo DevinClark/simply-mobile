80d9bb26ae8a53d3a6d5f8d44e7f987496560629
DevinClark <dclarkdesign@gmail.com>
2013-02-25 23:27:25 -0600

    Someone didn't indent their code.

--- js/app.js
+++ js/app.js
@@ -170,28 +170,29 @@ var Geolocation = {
 	}
 };
 
-var slideBeginX; 
-function touchStart(event){event.preventDefault();//always prevent default Safari actions
-    slideBeginX = event.targetTouches[0].pageX;
+var slideBeginX;
+function touchStart(event){
+	event.preventDefault();//always prevent default Safari actions
+	slideBeginX = event.targetTouches[0].pageX;
 };
 
 function touchMove(event) {
-event.preventDefault();
-// whatever you want to add here
+	event.preventDefault();
+	// whatever you want to add here
 };
 
 function touchEnd(event) {
-event.preventDefault();
-var slideEndX = event.changedTouches[0].pageX;
+	event.preventDefault();
+	var slideEndX = event.changedTouches[0].pageX;
 
-// Now add a minimum slide distance so that the links on the page are still clickable
-if (Math.abs(slideEndX - slideBeginX) > 200) {
-if (slideEndX - slideBeginX > 0) {
-// It means the user has scrolled from left to right
-} else {
-// It means the user has scrolled from right to left.
-};
-};
+	// Now add a minimum slide distance so that the links on the page are still clickable
+	if (Math.abs(slideEndX - slideBeginX) > 200) {
+		if (slideEndX - slideBeginX > 0) {
+		// It means the user has scrolled from left to right
+		} else {
+		// It means the user has scrolled from right to left.
+		};
+	};
 };
 
 var ShowNavigation = {
