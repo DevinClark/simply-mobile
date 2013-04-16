81c89d34dfe5de169e9274e19a9f3944d5f44ce2
DevinClark <dclarkdesign@gmail.com>
2013-04-15 21:20:48 -0500

    New Modernizr build, featuring less features than the previous build!

--- js/modernizr.js
+++ js/modernizr.js
@@ -1,5 +1,5 @@
 /* Modernizr 2.6.2 (Custom Build) | MIT & BSD
- * Build: http://modernizr.com/download/#-fontface-applicationcache-canvas-canvastext-draganddrop-hashchange-history-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websqldatabase-webworkers-geolocation-inlinesvg-smil-svg-svgclippaths-touch-webgl-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-a_download-audio_audiodata_api-audio_webaudio_api-battery_api-battery_level-contextmenu-dataview_api-dom_dataset-event_deviceorientation_motion-file_api-forms_fileinput-forms_placeholder-forms_speechinput-forms_validation-fullscreen_api-gamepad-getusermedia-mathml-network_connection-network_eventsource-network_xhr2-notification-performance-pointerlock_api-script_async-script_defer-unicode-userdata-vibration-web_intents-webgl_extensions
+ * Build: http://modernizr.com/download/#-applicationcache-audio-video-indexeddb-input-inputtypes-localstorage-postmessage-sessionstorage-websqldatabase-webworkers-geolocation-inlinesvg-svg-svgclippaths-touch-cssclasses-addtest-prefixed-teststyles-testprop-testallprops-hasevent-prefixes-domprefixes-getusermedia-json-network_connection
  */
 ;
 
@@ -11,6 +11,7 @@ window.Modernizr = (function( window, document, undefined ) {
 
     Modernizr = {},
 
+    enableClasses = true,
 
     docElement = document.documentElement,
 
@@ -44,7 +45,7 @@ window.Modernizr = (function( window, document, undefined ) {
 
     slice = classes.slice,
 
-    featureName,
+    featureName, 
 
 
     injectElementWithStyles = function( rule, callback, nodes, testnames ) {
@@ -133,7 +134,7 @@ window.Modernizr = (function( window, document, undefined ) {
       };
     }
     else {
-      hasOwnProp = function (object, property) {
+      hasOwnProp = function (object, property) { 
         return ((property in object) && is(object.constructor.prototype[property], 'undefined'));
       };
     }
@@ -236,27 +237,7 @@ window.Modernizr = (function( window, document, undefined ) {
           props = (prop + ' ' + (domPrefixes).join(ucProp + ' ') + ucProp).split(' ');
           return testDOMProps(props, prefixed, elem);
         }
-    }
-
-
-
-    tests['canvas'] = function() {
-        var elem = document.createElement('canvas');
-        return !!(elem.getContext && elem.getContext('2d'));
-    };
-
-    tests['canvastext'] = function() {
-        return !!(Modernizr['canvas'] && is(document.createElement('canvas').getContext('2d').fillText, 'function'));
-    };
-
-
-
-    tests['webgl'] = function() {
-        return !!window.WebGLRenderingContext;
-    };
-
-
-    tests['touch'] = function() {
+    }    tests['touch'] = function() {
         var bool;
 
         if(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
@@ -290,30 +271,6 @@ window.Modernizr = (function( window, document, undefined ) {
       return !!testPropsAll("indexedDB", window);
     };
 
-    tests['hashchange'] = function() {
-      return isEventSupported('hashchange', window) && (document.documentMode === undefined || document.documentMode > 7);
-    };
-
-    tests['history'] = function() {
-      return !!(window.history && history.pushState);
-    };
-
-    tests['draganddrop'] = function() {
-        var div = document.createElement('div');
-        return ('draggable' in div) || ('ondragstart' in div && 'ondrop' in div);
-    };    tests['fontface'] = function() {
-        var bool;
-
-        injectElementWithStyles('@font-face {font-family:"font";src:url("https://")}', function( node, rule ) {
-          var style = document.getElementById('smodernizr'),
-              sheet = style.sheet || style.styleSheet,
-              cssText = sheet ? (sheet.cssRules && sheet.cssRules[0] ? sheet.cssRules[0].cssText : sheet.cssText || '') : '';
-
-          bool = /src/i.test(cssText) && cssText.indexOf(rule.split(' ')[0]) === 0;
-        });
-
-        return bool;
-    };
 
     tests['video'] = function() {
         var elem = document.createElement('video'),
@@ -395,9 +352,6 @@ window.Modernizr = (function( window, document, undefined ) {
       return (div.firstChild && div.firstChild.namespaceURI) == ns.svg;
     };
 
-    tests['smil'] = function() {
-        return !!document.createElementNS && /SVGAnimate/.test(toString.call(document.createElementNS(ns.svg, 'animate')));
-    };
 
 
     tests['svgclippaths'] = function() {
@@ -487,7 +441,7 @@ window.Modernizr = (function( window, document, undefined ) {
 
        }
 
-       return Modernizr;
+       return Modernizr; 
      };
 
 
@@ -521,154 +475,25 @@ window.Modernizr = (function( window, document, undefined ) {
     };
 
 
+    docElement.className = docElement.className.replace(/(^|\s)no-js(\s|$)/, '$1$2') +
+
+                                                    (enableClasses ? ' js ' + classes.join(' ') : '');
 
     return Modernizr;
 
 })(this, this.document);
-// Mozilla Audio Data API
-// https://wiki.mozilla.org/Audio_Data_API
-// by Addy Osmani
-Modernizr.addTest('audiodata', !!(window.Audio));
-
-// a[download] attribute
-// When used on an <a>, this attribute signifies that the resource it
-// points to should be downloaded by the browser rather than navigating to it.
-// http://developers.whatwg.org/links.html#downloading-resources
-// By Addy Osmani
-
-Modernizr.addTest('adownload', 'download' in document.createElement('a'));
-
-// Grab the WebGL extensions currently supported and add to the Modernizr.webgl object
-// spec: www.khronos.org/registry/webgl/specs/latest/#5.13.14
-
-// based on code from ilmari heikkinen
-// code.google.com/p/graphics-detect/source/browse/js/detect.js
-
-
-(function(){
-
-    if (!Modernizr.webgl) return;
-
-    var canvas, ctx, exts;
-
-    try {
-        canvas  = document.createElement('canvas');
-        ctx     = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
-        exts    = ctx.getSupportedExtensions();
-    }
-    catch (e) {
-        return;
-    }
-
-    if (ctx === undefined) {
-        Modernizr.webgl = new Boolean(false);
-    }
-    else {
-        Modernizr.webgl = new Boolean(true);
-    }
-
-
-    for (var i = -1, len = exts.length; ++i < len; ){
-        Modernizr.webgl[exts[i]] = true;
-    }
-
-    // hack for addressing modernizr testsuite failures. sorry.
-    if (window.TEST && TEST.audvid){
-        TEST.audvid.push('webgl');
-    }
-
-    canvas = undefined;
-})();// Tests for the ability to use Web Intents (http://webintents.org).
-// By Eric Bidelman
-
-Modernizr.addTest('webintents', function() {
-  return !!Modernizr.prefixed('startActivity', navigator);
-});
-// Vibration API
-// http://www.w3.org/TR/vibration/
-// https://developer.mozilla.org/en/DOM/window.navigator.mozVibrate
-Modernizr.addTest('vibrate', !!Modernizr.prefixed('vibrate', navigator));// test if IE userdata supported
-// msdn.microsoft.com/en-us/library/ms531424(v=vs.85).aspx
-// test by @stereobooster
-
-Modernizr.addTest('userdata', function(){
-  return !!document.createElement('div').addBehavior;
-});
-/**
- * Unicode special character support
- *
- * Detection is made by testing missing glyph box rendering against star character
- * If widths are the same, this "probably" means the browser didn't support the star character and rendered a glyph box instead
- * Just need to ensure the font characters have different widths
- *
- * Warning : positive Unicode support doesn't mean you can use it inside <title>, this seams more related to OS & Language packs
- */
-Modernizr.addTest('unicode', function() {
-
-
-	var bool,
-
-		missingGlyph = document.createElement('span'),
-
-		star = document.createElement('span');
-
-	Modernizr.testStyles('#modernizr{font-family:Arial,sans;font-size:300em;}', function(node) {
-
-		missingGlyph.innerHTML = '&#5987';
-		star.innerHTML = '&#9734';
-
-		node.appendChild(missingGlyph);
-		node.appendChild(star);
-
-		bool = 'offsetWidth' in missingGlyph && missingGlyph.offsetWidth !== star.offsetWidth;
-	});
-
-	return bool;
-
-});// defer script
-// By Theodoor van Donge
-Modernizr.addTest('scriptdefer', 'defer' in document.createElement('script'));// async script
-// By Theodoor van Donge
-Modernizr.addTest('scriptasync', 'async' in document.createElement('script'));// https://developer.mozilla.org/en-US/docs/API/Pointer_Lock_API
-
-Modernizr.addTest('pointerlock',!!Modernizr.prefixed('pointerLockElement', document));
-
-// Navigation Timing (Performance)
-// https://dvcs.w3.org/hg/webperf/raw-file/tip/specs/NavigationTiming/
-// http://www.html5rocks.com/en/tutorials/webperformance/basics/
-// By Scott Murphy (uxder)
-Modernizr.addTest('performance', !!Modernizr.prefixed('performance', window));// Notifications
-// By Theodoor van Donge
-
-// window.webkitNotifications is only used by Chrome
-//	http://www.html5rocks.com/en/tutorials/notifications/quick/
-
-// window.Notification only exist in the draft specs
-//	http://dev.w3.org/2006/webapi/WebNotifications/publish/Notifications.html#idl-if-Notification
-
-Modernizr.addTest('notification', !!Modernizr.prefixed('Notifications', window));
-
-// XML HTTP Request Level 2
-// www.w3.org/TR/XMLHttpRequest2/
-
-// Much more details at github.com/Modernizr/Modernizr/issues/385
-
-// all three of these details report consistently across all target browsers:
-//   !!(window.ProgressEvent);
-//   !!(window.FormData);
-//   window.XMLHttpRequest && "withCredentials" in new XMLHttpRequest;
-
-Modernizr.addTest('xhr2', 'FormData' in window);
-
-// server sent events aka eventsource
-// dev.w3.org/html5/eventsource/
-
-Modernizr.addTest('eventsource', !!window.EventSource);
 // getUserMedia
 // http://www.whatwg.org/specs/web-apps/current-work/multipage/video-conferencing-and-peer-to-peer-communication.html
 // By Eric Bidelman
 
-Modernizr.addTest('getusermedia', !!Modernizr.prefixed('getUserMedia', navigator));// determining low-bandwidth via navigator.connection
+Modernizr.addTest('getusermedia', !!Modernizr.prefixed('getUserMedia', navigator));// native JSON support.
+// developer.mozilla.org/en/JSON
+
+// this will also succeed if you've loaded the JSON2.js polyfill ahead of time
+//   ... but that should be obvious. :)
+
+Modernizr.addTest('json', !!window.JSON && !!JSON.parse);
+// determining low-bandwidth via navigator.connection
 
 // There are two iterations of the navigator.connection interface:
 
@@ -689,218 +514,5 @@ Modernizr.addTest('lowbandwidth', function() {
   return connection.type == 3 || // connection.CELL_2G
       connection.type == 4 || // connection.CELL_3G
       /^[23]g$/.test(connection.type); // string value in new spec
-});
-// GamePad API
-// https://dvcs.w3.org/hg/gamepad/raw-file/default/gamepad.html
-// By Eric Bidelman
-
-// FF has Gamepad API support only in special builds, but not in any release (even behind a flag)
-// Their current implementation has no way to feature detect, only events to bind to.
-//   http://www.html5rocks.com/en/tutorials/doodles/gamepad/#toc-featuredetect
-
-// but a patch will bring them up to date with the spec when it lands (and they'll pass this test)
-//   https://bugzilla.mozilla.org/show_bug.cgi?id=690935
-
-Modernizr.addTest('gamepads', !!Modernizr.prefixed('getGamepads', navigator));
-Modernizr.addTest('fullscreen',function(){
-     for(var i = 0; i < Modernizr._domPrefixes.length; i++) {
-        if( document[Modernizr._domPrefixes[i].toLowerCase() + 'CancelFullScreen'])
-            return true;
-     }
-     return !!document['cancelFullScreen'] || false;
-});
-
-// http://developer.apple.com/library/safari/documentation/AudioVideo/Conceptual/Using_HTML5_Audio_Video/ControllingMediaWithJavaScript/ControllingMediaWithJavaScript.html#//apple_ref/doc/uid/TP40009523-CH3-SW20
-// https://developer.mozilla.org/en/API/Fullscreen
-// This implementation only tests support for interactive form validation.
-// To check validation for a specific type or a specific other constraint,
-// the test can be combined:
-//    - Modernizr.inputtypes.numer && Modernizr.formvalidation (browser supports rangeOverflow, typeMismatch etc. for type=number)
-//    - Modernizr.input.required && Modernizr.formvalidation (browser supports valueMissing)
-//
-(function(document, Modernizr){
-
-
-Modernizr.formvalidationapi = false;
-Modernizr.formvalidationmessage = false;
-
-Modernizr.addTest('formvalidation', function(){
-    var form = document.createElement('form');
-    if ( !('checkValidity' in form) ) {
-        return false;
-    }
-    var body = document.body,
-
-    html = document.documentElement,
-
-    bodyFaked = false,
-
-    invaildFired = false,
-
-    input;
-
-    Modernizr.formvalidationapi = true;
-
-    // Prevent form from being submitted
-    form.onsubmit = function(e) {
-        //Opera does not validate form, if submit is prevented
-        if ( !window.opera ) {
-            e.preventDefault();
-        }
-        e.stopPropagation();
-    };
-
-    // Calling form.submit() doesn't trigger interactive validation,
-    // use a submit button instead
-    //older opera browsers need a name attribute
-    form.innerHTML = '<input name="modTest" required><button></button>';
-
-    // FF4 doesn't trigger "invalid" event if form is not in the DOM tree
-    // Chrome throws error if invalid input is not visible when submitting
-    form.style.position = 'absolute';
-    form.style.top = '-99999em';
-
-    // We might in <head> in which case we need to create body manually
-    if ( !body ) {
-        bodyFaked = true;
-        body = document.createElement('body');
-        //avoid crashing IE8, if background image is used
-        body.style.background = "";
-        html.appendChild(body);
-    }
-
-    body.appendChild(form);
-
-    input = form.getElementsByTagName('input')[0];
-
-    // Record whether "invalid" event is fired
-    input.oninvalid = function(e) {
-        invaildFired = true;
-        e.preventDefault();
-        e.stopPropagation();
-    };
-
-    //Opera does not fully support the validationMessage property
-    Modernizr.formvalidationmessage = !!input.validationMessage;
-
-    // Submit form by clicking submit button
-    form.getElementsByTagName('button')[0].click();
-
-    // Don't forget to clean up
-    body.removeChild(form);
-    bodyFaked && html.removeChild(body);
-
-    return invaildFired;
-});
-
-
-})(document, window.Modernizr);// speech input for inputs
-// by @alrra
-
-
-// `webkitSpeech` in elem
-// doesn`t work correctly in all versions of Chromium based browsers.
-//   It can return false even if they have support for speech i.imgur.com/2Y40n.png
-//  Testing with 'onwebkitspeechchange' seems to fix this problem
-
-// this detect only checks the webkit version because
-// the speech attribute is likely to be deprecated in favor of a JavaScript API.
-// http://lists.w3.org/Archives/Public/public-webapps/2011OctDec/att-1696/speechapi.html
-
-// FIXME: add support for detecting the new spec'd behavior
-
-Modernizr.addTest('speechinput', function(){
-    var elem = document.createElement('input');
-    return 'speech' in elem || 'onwebkitspeechchange' in elem;
-});
-
-// Detects whether input type="file" is available on the platform
-// E.g. iOS < 6 and some android version don't support this
-
-//  It's useful if you want to hide the upload feature of your app on devices that
-//  don't support it (iphone, ipad, etc).
-
-Modernizr.addTest('fileinput', function() {
-    var elem = document.createElement('input');
-    elem.type = 'file';
-    return !elem.disabled;
-});
-// testing for placeholder attribute in inputs and textareas
-// re-using Modernizr.input if available
-
-Modernizr.addTest('placeholder', function(){
-
-  return !!( 'placeholder' in ( Modernizr.input    || document.createElement('input')    ) &&
-             'placeholder' in ( Modernizr.textarea || document.createElement('textarea') )
-           );
-
-});
-/**
- * file tests for the File API specification
- *   Tests for objects specific to the File API W3C specification without
- *   being redundant (don't bother testing for Blob since it is assumed
- *   to be the File object's prototype.
- *
- *   Will fail in Safari 5 due to its lack of support for the standards
- *   defined FileReader object
- */
-Modernizr.addTest('filereader', function () {
-    return !!(window.File && window.FileList && window.FileReader);
-});
-//By Shi Chuan
-//Part of Device Access aspect of HTML5, same category as geolocation
-//W3C Editor's Draft at http://dev.w3.org/geo/api/spec-source-orientation.html
-//Implementation by iOS Safari at http://goo.gl/fhce3 and http://goo.gl/rLKz8
-
-
-//test for Device Motion Event support, returns boolean value true/false
-Modernizr.addTest('devicemotion', ('DeviceMotionEvent' in window) );
-
-//test for Device Orientation Event support, returns boolean value true/false
-Modernizr.addTest('deviceorientation', ('DeviceOrientationEvent' in window) );
-
-// dataset API for data-* attributes
-// test by @phiggins42
-
-Modernizr.addTest('dataset', function(){
-  var n = document.createElement("div");
-  n.setAttribute("data-a-b", "c");
-  return !!(n.dataset && n.dataset.aB === "c");
-});
-// DataView
-// https://developer.mozilla.org/en/JavaScript_typed_arrays/DataView
-// By Addy Osmani
-Modernizr.addTest('dataview', (typeof DataView !== 'undefined' && 'getFloat64' in DataView.prototype));// Web Audio API
-// https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html
-// By Addy Osmani
-Modernizr.addTest('webaudio', !!(window.webkitAudioContext || window.AudioContext));
-
-// Battery API
-// https://developer.mozilla.org/en/DOM/window.navigator.mozBattery
-// By: Paul Sayre
-
-Modernizr.addTest('battery',
-	!!Modernizr.prefixed('battery', navigator)
-);// http://www.w3.org/TR/html5/interactive-elements.html#context-menus
-// Demo at http://thewebrocks.com/demos/context-menu/
-Modernizr.addTest(
-  'contextmenu',
-  ('contextMenu' in document.documentElement && 'HTMLMenuItemElement' in window)
-);
-
-
-
-
-
-
-// Low Battery Level
-// Enable a developer to remove CPU intensive CSS/JS when battery is low
-// developer.mozilla.org/en/DOM/window.navigator.mozBattery
-// By: Paul Sayre
-
-Modernizr.addTest('lowbattery', function () {
-	var minLevel = 0.20,
-		battery = Modernizr.prefixed('battery', navigator);
-	return !!(battery && !battery.charging && battery.level <= minLevel);
 });
 ;
